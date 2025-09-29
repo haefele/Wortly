c2875aedcecfd120b6c950f678e767f873535857
Daniel Häfele <haefele@xemio.net>
2025-09-29 20:28:05 +0200

    Add word-type filter to searchtext as well

--- convex/migrations.ts
+++ convex/migrations.ts
@@ -1,6 +1,7 @@
 import { Migrations } from "@convex-dev/migrations";
 import { DataModel } from "./_generated/dataModel";
 import { components, internal } from "./_generated/api";
+import { getSearchText } from "./lib/wordboxHelpers";
 
 export const migrations = new Migrations<DataModel>(components.migrations);
 
@@ -21,4 +22,20 @@ export const updateWordTypeInAssignments = migrations.define({
 
 export const runUpdateWordTypeInAssignments = migrations.runner(
   internal.migrations.updateWordTypeInAssignments
+);
+
+export const updateSearchTextInAssignments = migrations.define({
+  table: "wordBoxAssignments",
+  migrateOne: async (ctx, row) => {
+    const word = await ctx.db.get(row.wordId);
+    if (!word) {
+      return {};
+    }
+
+    return { searchText: getSearchText(word) };
+  },
+});
+
+export const runUpdateSearchTextInAssignments = migrations.runner(
+  internal.migrations.updateSearchTextInAssignments
 );
