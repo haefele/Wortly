40987116e968b8354063a2c335c0dcd475039f44
Daniel Häfele <haefele@xemio.net>
2025-10-04 18:12:20 +0200

    Small thingy

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -131,7 +131,7 @@ export default function LearnPage() {
                     </>
                   ) : (
                     <>
-                      <ArrowRight /> Load more sessions
+                      <ChevronDown /> Load more sessions
                     </>
                   )}
                 </Button>
